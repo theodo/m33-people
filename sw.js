5dfd7519cad86ed1aef6cf0af9f03d93ce32c9e0
nicgirault <nic.girault@gmail.com>
2017-01-19 23:01:38 +0100

    add service worker

---
+++ sw.js
@@ -0,0 +1,43 @@
+
+var current_version = 'ta-dir-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(current_version).then(cache => {
+      return cache.addAll([
+        '/ta-dir/',
+        '/ta-dir/index.html',
+        '/ta-dir/dist/app.css',
+        '/ta-dir/dist/jquery.min.js',
+        '/ta-dir/dist/app.js',
+        '/ta-dir/dist/trello.js',
+        '/ta-dir/icons/favicon-16x16.png',
+        '/ta-dir/icons/favicon-32x32.png',
+        '/ta-dir/icons/favicon-96x96.png',
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [current_version];
+
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
